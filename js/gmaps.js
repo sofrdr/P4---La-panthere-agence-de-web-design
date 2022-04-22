87af2ed50fef0f57cfe8b2a051356375281199d4
Sophie Roudier <sophie.roudier@hotmail.fr>
2022-04-22 12:38:13 +0200

    Suppresion streetView + ajout lien nav portfolio

--- js/gmaps.js
+++ js/gmaps.js
@@ -3,7 +3,9 @@ function initMap() {
   // The location of laPanthere
   const laPanthere = { lat: 45.76968807047258, lng: 4.830674599999999 };
   // The map, centered at laPanthere
-  const map = new google.maps.Map(document.getElementById("map"), {
+
+  const map = new google.maps.Map(document.getElementById("map"),  {
+    streetViewControl: false,
     zoom: 15,
     center: laPanthere,
   });
